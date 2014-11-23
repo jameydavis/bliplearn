f598add3c7b0e7ab4baaea23bcc9f11ec2f37df0
Dougferd <Dmurray@dougferd.com>
2014-11-23 16:52:27 -0600

    Changed anchor tags on the splash page, eventually need to change to rails syntax. Also edited routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  #root to: 'visitors#index'
+  root to: 'splash#index'
   devise_for :users
   resources :users
+  get '/courses', to: 'courses#index'
 end
